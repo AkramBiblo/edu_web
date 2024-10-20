d57c10ec61d53072892cf55a01702fd4dd0c1337
Akram Biblo <akram.biblo@gmail.com>
2024-10-20 03:24:39 -0700

    Blank Setup

---
+++ src/routes/router.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+let cookieParser = require("cookie-parser");
+router.use(cookieParser());
+
+router.get("/", (req, res) => {
+    res.render("home", {title: "Home"})
+})
+
+module.exports = router;
